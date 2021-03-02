8e08ade45b2dfc7f60f21477479258ed48d7ad79
tomguyperson <57741877+tomguyperson@users.noreply.github.com>
2021-03-01 19:41:53 -0700

    Added names and variables to gitdemo
    My name was added to line 2, added a reverseNum variable, and modified the add number function

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+    Tomas Mesquita
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,6 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
+        previousNumber += num;
         return 0;
     }
     
@@ -45,6 +47,8 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
+        
+        reverseNum = newNumber;
         
         return newNumber;
     }
